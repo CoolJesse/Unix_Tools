2f4de841d60a81b91186c0fb72983af4d90a6112
Jesse Olson <jesstolson@gmail.com>
2018-05-14 18:53:35 -0400

    Update practice.c

--- project_2/practice.c
+++ project_2/practice.c
@@ -4,7 +4,8 @@
 #include <fcntl.h>
 #include <errno.h>
 
-#define BUFFSIZE 8192
+#define INPUT_BUFF_SIZE 1024
+#define FILE_BUFF_SIZE 8192
 #define NUMBER_OF_BOOKS 3
 
 void clear_char_buffer(char buffer[], int max_size)
@@ -16,14 +17,17 @@ void clear_char_buffer(char buffer[], int max_size)
 
 int search(char *file_name, char *book_info, int* byte_position);
 int insert(int file_desc, char *file_name, char *book_info);
-int delete(int file_desc, char *file_name, char *book_info);
-int replace(int file_desc, char *file_name, char *book_info);
+int delete(int file_desc, char *file_name, char *book_info, char buffer_1[], char buffer_2[]);
+int replace(int file_desc, char *file_name, char *book_info, char buffer_1[], char buffer_2[]);
 
 int main(int argc, char *argv[])
 {
 
 	int n, comp, fd, byte_position; 
-	char buffer[BUFFSIZE];
+
+	char input_buffer[INPUT_BUFF_SIZE];
+	char file_buffer_1[FILE_BUFF_SIZE];
+	char file_buffer_2[FILE_BUFF_SIZE];
 
 	char *filename = "books.txt"; 
 	char *prompt = "Please enter a command: SEARCH, INSERT, DELETE, REPLACE, or QUIT> ";
@@ -57,21 +61,22 @@ int main(int argc, char *argv[])
 	/** prompt user to enter command and handle input *********************************************/
 	bytesWritten = write(STDOUT_FILENO, prompt, strlen(prompt));
 	
-	clear_char_buffer(buffer, BUFFSIZE);
-	while( (n=read(STDIN_FILENO, buffer, BUFFSIZE)) > 0)
+	clear_char_buffer(input_buffer, INPUT_BUFF_SIZE);
+
+	while( (n=read(STDIN_FILENO, input_buffer, INPUT_BUFF_SIZE)) > 0)
 	{
-		if(write(STDOUT_FILENO, buffer, n) != n)
+		if(write(STDOUT_FILENO, input_buffer, n) != n)
 			printf("WRITE ERROR\n");
 
 		/** search command ***********************************/
-		if( strncmp(buffer, "SEARCH", 6) == 0)
+		if( strncmp(input_buffer, "SEARCH", 6) == 0)
 		{
 			printf("You have entered SEARCH, %s\n", book_info_prompt);
 
-			if( (read(STDIN_FILENO, buffer, BUFFSIZE) > 0) )
+			if( (read(STDIN_FILENO, input_buffer, INPUT_BUFF_SIZE) > 0) )
 			{
 
-				if( (search(filename, buffer, &byte_position)==1) )
+				if( (search(filename, input_buffer, &byte_position)==1) )
 					printf("Found in record at byte position: %d.\n", byte_position);
 
 		
@@ -84,42 +89,42 @@ int main(int argc, char *argv[])
 		}
 		
 		/** insert command ***********************************/
-		else if( strncmp(buffer, "INSERT", 6) == 0)
+		else if( strncmp(input_buffer, "INSERT", 6) == 0)
 		{
 			printf("You have entered INSERT, %s\n", book_info_prompt);
 
-			if(read(STDIN_FILENO, buffer, BUFFSIZE) >0)
-				insert(fd, filename, buffer);
+			if(read(STDIN_FILENO, input_buffer, INPUT_BUFF_SIZE) >0)
+				insert(fd, filename, input_buffer);
 			
 			else
 				printf("Sorry, you did not enter any data to insert into the file.\n");
 		}
 
 		/** delete command **********************************/
-		else if( (comp = strncmp(buffer, "DELETE", 6)) == 0)
+		else if( (comp = strncmp(input_buffer, "DELETE", 6)) == 0)
 		{
 			printf("You have entered DELETE, %s\n", book_info_prompt);
 
-			if(read(STDIN_FILENO, buffer, BUFFSIZE) > 0)
-				delete(fd, filename, buffer);
+			if(read(STDIN_FILENO, input_buffer, INPUT_BUFF_SIZE) > 0)
+				delete(fd, filename, input_buffer, file_buffer_1, file_buffer_2);
 
 			else
 				printf("Sorry, you did not enter any data to insert into the file.\n");				
 		}
 		
 		/** replace command **********************************/
-		else if( (comp = strncmp(buffer, "REPLACE", 7)) == 0)
+		else if( (comp = strncmp(input_buffer, "REPLACE", 7)) == 0)
 		{
 			printf("You have entered REPLACE, %s\n", book_info_prompt);
 
-			if(read(STDIN_FILENO, buffer, BUFFSIZE) > 0)
-				replace(fd, filename, buffer);
+			if(read(STDIN_FILENO, input_buffer, INPUT_BUFF_SIZE) > 0)
+				replace(fd, filename, input_buffer, file_buffer_1, file_buffer_1);
 			else
 				printf("Sorry, you did not enter any data to insert into the file.\n");
 		}
 
 		/** quit command *************************************/
-		else if( (comp = strncmp(buffer, "QUIT", 4)) == 0)
+		else if( (comp = strncmp(input_buffer, "QUIT", 4)) == 0)
 		{
 			printf("You have entered QUIT, goodbye!\n");
 			break;
@@ -129,7 +134,7 @@ int main(int argc, char *argv[])
 		else
 			printf("Sorry, you have entered an invalid command\n");
 
-		clear_char_buffer(buffer, BUFFSIZE);
+		clear_char_buffer(input_buffer, INPUT_BUFF_SIZE);
 		write(STDOUT_FILENO, prompt, strlen(prompt));
 	}
 	/*****************************************************************************************************/
@@ -192,23 +197,78 @@ int insert(int file_desc, char *file_name, char *book_info)
 
 	return success;
 }   
-int delete(int file_desc, char* file_name, char *book_info)
+int delete(int file_desc, char* file_name, char *book_info, char buffer_1[], char buffer_2[])
 {
 	int byte_position = 0;
-	if( search(file_name, book_info, &byte_position) == 1)
+
+	clear_char_buffer(buffer_1, FILE_BUFF_SIZE);
+	clear_char_buffer(buffer_2, FILE_BUFF_SIZE);
+
+	if( search(file_name, book_info, &byte_position) == 1) //entry exists in file to delete
 	{
-		printf("Delete entry at byte_position: %d\n", byte_position);
+		if(pread(file_desc, buffer_1, byte_position, 0)< 0) //read up to record to be replaced
+			printf("Error reading from file number %d\n", file_desc);
+
+		if(pread(file_desc, buffer_2, FILE_BUFF_SIZE, strlen(book_info) + byte_position)<0)
+			printf("Error reading from file number %d\n", file_desc);
+
+		else
+		{
+			printf("buffer_1 %s\n", buffer_1);
+			printf("buffer_2 %s\n", buffer_2);
+
+			if(pwrite(file_desc, buffer_1, strlen(buffer_1), 0) < strlen(buffer_1) )
+				printf("Error writing to file %d", file_desc);
+
+			if(pwrite(file_desc, buffer_2, strlen(buffer_2), strlen(buffer_1)) < strlen(buffer_2) )
+				printf("Error writing to file %d", file_desc);
+
+			printf("Delete entry at byte_position: %d\n", byte_position);
+		}	
 	}
 	
 	else
 		printf("The record you are attempting to delete does not exist.\n");
 }
-int replace(int file_desc, char *file_name, char *book_info)
+int replace(int file_desc, char *file_name, char *book_info, char buffer_1[], char buffer_2[])
 {
 	int byte_position = 0;
-	if( search(file_name, book_info, &byte_position) == 1)
+	char replacement_entry[INPUT_BUFF_SIZE];
+
+	clear_char_buffer(buffer_1, FILE_BUFF_SIZE);
+	clear_char_buffer(buffer_2, FILE_BUFF_SIZE);
+
+	if( search(file_name, book_info, &byte_position) == 1) //entry exists in file to delete
 	{
-		printf("replace entry at byte position: %d\n", byte_position);
+		if(read(STDIN_FILENO, replacement_entry, INPUT_BUFF_SIZE)) > 0)
+		{
+			printf("Error reading entry to replace with, error: %s\n", strerror(errno));
+			break;
+		}		
+
+		if(pread(file_desc, buffer_1, byte_position, 0)< 0) //read up to record to be replaced
+			printf("Error reading from file number %d\n", file_desc);
+
+		if(pread(file_desc, buffer_2, FILE_BUFF_SIZE, strlen(book_info) + byte_position)<0)
+			printf("Error reading from file number %d\n", file_desc);
+
+		else
+		{
+			printf("buffer_1 %s\n", buffer_1);
+			printf("replacement text: %s\n", book_info);
+			printf("buffer_2 %s\n", buffer_2);
+
+			if(pwrite(file_desc, buffer_1, strlen(buffer_1), 0) < strlen(buffer_1) )
+				printf("Error writing to file %d", file_desc);
+
+			if(pwrite(file_desc, book_info, strlen(book_info), strlen(buffer_1)) < strlen(book_info) )
+				printf("Error writing replacement entry to file %d\n", file_desc);
+
+			if(pwrite(file_desc, buffer_2, strlen(buffer_2), strlen(buffer_1)) < strlen(buffer_2) )
+				printf("Error writing to file %d", file_desc);
+
+			printf("Replaced entry at byte_position: %d\n", byte_position);
+		}	
 	}
 	
 	else
