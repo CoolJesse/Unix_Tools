0da7ec0b809367b3826467a97f047a3158e0ad5c
Jesse Olson <jesstolson@gmail.com>
2018-05-14 14:25:02 -0400

    Update practice.c

--- project_2/practice.c
+++ project_2/practice.c
@@ -5,6 +5,7 @@
 #include <errno.h>
 
 #define BUFFSIZE 8192
+#define NUMBER_OF_BOOKS 20
 
 int search(char *file_name, char *book_info);
 int insert(int file_desc, char *file_name, char *book_info);
@@ -21,15 +22,32 @@ int main(int argc, char *argv[])
 	char *prompt = "Please enter a command: SEARCH, INSERT, DELETE, REPLACE, or QUIT> ";
 	char *book_info_prompt = "Please enter the title and author info in the following format: BookTitle AuthorName";	
 
-	/*********************************** Create file to write to **********************************/
+	/** Create file to write to *******************************************************************/
 	mode_t mode = S_IRWXU; //This is equivalent to ‘(S_IRUSR | S_IWUSR | S_IXUSR)
+
 	if((fd = open("books.txt", O_CREAT|O_RDWR|O_TRUNC, mode)) == -1)
 		fprintf(stderr, "Error opening/creating file: %s\n", strerror(errno));
 	else
 		printf("File opened/created successfully.\n");
 	/**********************************************************************************************/
+	/** write book titles to file *****************************************************************/
+	ssize_t bytesWritten, bytesRead;
+	const char *books[NUMBER_OF_BOOKS];
 
-	write(STDOUT_FILENO, prompt, strlen(prompt));
+	books[0] = "EastOfEden JohnSteinbeck\n";
+	books[1] = "FiresOfHeaven RobertJordan\n";
+	books[2] = "BloodMeridian CormackMcCarthy\n";
+	/*
+	for(int i=0; i<NUMBER_OF_BOOKS; i++)
+	{	if((bytesWritten = write(fd, books[i], strlen(books[i]))) < strlen(books[i]))
+			fprintf(stderr, "Error writing to file: %s\n", strerror(errno));
+		else
+			;//printf("Data printed to file %d successfully: %s", fileDesc, books[i]);
+	}
+	*/
+	/**********************************************************************************************/
+	/** prompt user to enter command and handle input *********************************************/
+	bytesWritten = write(STDOUT_FILENO, prompt, strlen(prompt));
 
 	while( (n=read(STDIN_FILENO, buffer, BUFFSIZE)) > 0)
 	{
@@ -39,34 +57,26 @@ int main(int argc, char *argv[])
 		/** search command ***********************************/
 		if( strncmp(buffer, "SEARCH", 6) == 0)
 		{
-			printf("You have entered SEARCH\n");
-			printf("%s", book_info_prompt);
+			printf("You have entered SEARCH, %s\n", book_info_prompt);
 
 			if( (read(STDIN_FILENO, buffer, BUFFSIZE) > 0) )
 			{
 
 				if( (search(filename, buffer)!=0) )
-				{
-					printf("Found %s in record.\n", buffer);
-				}
-
+					printf("Found in record.\n");
+		
 				else
-				{
-					printf("%s not found in record.\n", buffer);
-				}
+					printf("Not found in record.\n");
 			}
 
 			else
-			{
 				printf("Sorry, you did not enter any data to search for in the file.\n");
-			}
 		}
 		
 		/** insert command ***********************************/
 		else if( strncmp(buffer, "INSERT", 6) == 0)
 		{
-			printf("You have entered INSERT\n");
-			printf("%s", book_info_prompt);
+			printf("You have entered INSERT, %s\n", book_info_prompt);
 
 			if(read(STDIN_FILENO, buffer, BUFFSIZE) >0)
 				insert(fd, filename, buffer);
@@ -78,25 +88,30 @@ int main(int argc, char *argv[])
 		/** delete command **********************************/
 		else if( (comp = strncmp(buffer, "DELETE", 6)) == 0)
 		{
-			printf("You have entered DELETE\n");
-			printf("%s", book_info_prompt);
+			printf("You have entered DELETE, %s\n", book_info_prompt);
 
-			read(STDIN_FILENO, buffer, BUFFSIZE);
+			if(read(STDIN_FILENO, buffer, BUFFSIZE) > 0)
+				delete(fd, filename, buffer);
+
+			else
+				printf("Sorry, you did not enter any data to insert into the file.\n");				
 		}
 		
 		/** replace command **********************************/
 		else if( (comp = strncmp(buffer, "REPLACE", 7)) == 0)
 		{
-			printf("You have entered REPLACE\n");
-			printf("%s", book_info_prompt);
+			printf("You have entered REPLACE, %s\n", book_info_prompt);
 
-			read(STDIN_FILENO, buffer, BUFFSIZE);
+			if(read(STDIN_FILENO, buffer, BUFFSIZE) > 0)
+				replace(fd, filename, buffer);
+			else
+				printf("Sorry, you did not enter any data to insert into the file.\n");
 		}
 
 		/** quit command *************************************/
 		else if( (comp = strncmp(buffer, "QUIT", 4)) == 0)
 		{
-			printf("You have entered QUIT\n");
+			printf("You have entered QUIT, goodbye!\n");
 			break;
 		}
 
@@ -106,7 +121,7 @@ int main(int argc, char *argv[])
 
 		write(STDOUT_FILENO, prompt, strlen(prompt));
 	}
-
+	/*****************************************************************************************************/
 	if(n<0)
 		printf("READ ERROR");
 
