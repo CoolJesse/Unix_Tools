f38f9d3e7ad0bd14c690de37ba12107c85b31c93
Jesse Olson <jesstolson@gmail.com>
2018-05-14 16:04:28 -0400

    Update practice.c

--- project_2/practice.c
+++ project_2/practice.c
@@ -5,9 +5,16 @@
 #include <errno.h>
 
 #define BUFFSIZE 8192
-#define NUMBER_OF_BOOKS 20
+#define NUMBER_OF_BOOKS 3
 
-int search(char *file_name, char *book_info);
+void clear_char_buffer(char buffer[], int max_size)
+{
+	size_t end_of_string = strlen(buffer) +1; //include null terminating character
+	for(int i=0; i< max_size && i < end_of_string; i++)
+		buffer[i] = 0;
+}
+
+int search(char *file_name, char *book_info, int* byte_position);
 int insert(int file_desc, char *file_name, char *book_info);
 int delete(int file_desc, char *file_name, char *book_info);
 int replace(int file_desc, char *file_name, char *book_info);
@@ -15,7 +22,7 @@ int replace(int file_desc, char *file_name, char *book_info);
 int main(int argc, char *argv[])
 {
 
-	int n, comp, fd; 
+	int n, comp, fd, byte_position; 
 	char buffer[BUFFSIZE];
 
 	char *filename = "books.txt"; 
@@ -37,18 +44,20 @@ int main(int argc, char *argv[])
 	books[0] = "EastOfEden JohnSteinbeck\n";
 	books[1] = "FiresOfHeaven RobertJordan\n";
 	books[2] = "BloodMeridian CormackMcCarthy\n";
-	/*
-	for(int i=0; i<NUMBER_OF_BOOKS; i++)
-	{	if((bytesWritten = write(fd, books[i], strlen(books[i]))) < strlen(books[i]))
+	
+	for(int i=0; i < NUMBER_OF_BOOKS; i++)
+	{	
+		if((bytesWritten = write(fd, books[i], strlen(books[i]))) < strlen(books[i]))
 			fprintf(stderr, "Error writing to file: %s\n", strerror(errno));
 		else
-			;//printf("Data printed to file %d successfully: %s", fileDesc, books[i]);
+			printf("Data printed to file %d successfully: %s", fd, books[i]);
 	}
-	*/
+	
 	/**********************************************************************************************/
 	/** prompt user to enter command and handle input *********************************************/
 	bytesWritten = write(STDOUT_FILENO, prompt, strlen(prompt));
-
+	
+	clear_char_buffer(buffer, BUFFSIZE);
 	while( (n=read(STDIN_FILENO, buffer, BUFFSIZE)) > 0)
 	{
 		if(write(STDOUT_FILENO, buffer, n) != n)
@@ -62,8 +71,9 @@ int main(int argc, char *argv[])
 			if( (read(STDIN_FILENO, buffer, BUFFSIZE) > 0) )
 			{
 
-				if( (search(filename, buffer)!=0) )
-					printf("Found in record.\n");
+				if( (search(filename, buffer, &byte_position)==1) )
+					printf("Found in record at byte position: %d.\n", byte_position);
+
 		
 				else
 					printf("Not found in record.\n");
@@ -119,6 +129,7 @@ int main(int argc, char *argv[])
 		else
 			printf("Sorry, you have entered an invalid command\n");
 
+		clear_char_buffer(buffer, BUFFSIZE);
 		write(STDOUT_FILENO, prompt, strlen(prompt));
 	}
 	/*****************************************************************************************************/
@@ -127,37 +138,50 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
-int search(char *file_name, char *book_info)
+int search(char *file_name, char *book_info, int *byte_position)
 {
 	FILE* fp;
 	
 	char book_info_comp[256];
 
-	int found = 0; //not yet found
-	int byte_position = 0;
+	//int byte_position = 0;
+	*byte_position = 0;
 
 	fp = fopen(file_name, "r+"); //opens file to update both reading and writing, file must exist
+	if(fp==NULL)
+	{
+		printf("Error opening file: %s\n", strerror(errno));
+		//return byte_position;
+		return -1;
+	}
+
+	//printf("Database contents:\n");
 
 	while( fgets(book_info_comp, 256, fp) != NULL) //reads entire line from text file
 	{ 
-		//fscanf(fp, "%s", book_author_comp); //parses buffer into seperate author and title
-		//fscanf(fp, "%s", book_title_comp);
+		//printf("Searching for: %s, Found: %s at byte position: %d\n", book_info, book_info_comp, *byte_position);
 
 		if( strncmp(book_info, book_info_comp, strlen(book_info)) == 0 )
-			return byte_position;
+		{
+			fclose(fp);
+			return 1;
+		}
 
 		else
-			byte_position += strlen(book_info);
+			*byte_position += strlen(book_info_comp);
 	}
 
-	byte_position = 0; //record is not found
-	return byte_position;
+	//byte_position = 0; //record is not found
+	fclose(fp);
+	//return byte_position;
+	return -1;
 }
 int insert(int file_desc, char *file_name, char *book_info)
 {
 	int success = 0;
+	int byte_position = 0;
 
-	if( search(file_name, book_info) == 0 )
+	if( search(file_name, book_info, &byte_position) == -1 )
 	{
 		pwrite(file_desc, book_info, strlen(book_info), lseek(file_desc, 0, SEEK_END));
 		success = 1;
@@ -171,9 +195,9 @@ int insert(int file_desc, char *file_name, char *book_info)
 int delete(int file_desc, char* file_name, char *book_info)
 {
 	int byte_position = 0;
-	if( (byte_position = search(file_name, book_info)) != 0)
+	if( search(file_name, book_info, &byte_position) == 1)
 	{
-		printf("Delete function here\n");
+		printf("Delete entry at byte_position: %d\n", byte_position);
 	}
 	
 	else
@@ -182,9 +206,9 @@ int delete(int file_desc, char* file_name, char *book_info)
 int replace(int file_desc, char *file_name, char *book_info)
 {
 	int byte_position = 0;
-	if( (byte_position = search(file_name, book_info)) != 0)
+	if( search(file_name, book_info, &byte_position) == 1)
 	{
-		printf("replace function here.\n");
+		printf("replace entry at byte position: %d\n", byte_position);
 	}
 	
 	else
