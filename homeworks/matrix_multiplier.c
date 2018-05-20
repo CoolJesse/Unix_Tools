fe53b882758fe8390c83df447ffd7f3e1814c474
Jesse Olson <jesstolson@gmail.com>
2018-05-20 13:59:49 -0400

    Update matrix_multiplier.c

--- homeworks/matrix_multiplier.c
+++ homeworks/matrix_multiplier.c
@@ -23,13 +23,55 @@ struct matrix_multiplication_struct
 	int (*array2)[row_B_size][column_B_size];
 	int (*array3)[row_A_size][column_B_size];
 
-	int row_index;
+	int row_index; //row index of array1, column index of array2
 };
 /**********************************************/
 
 int main(void)
 {
 
+	/** new code *********************************************************************/
+	time_t t;
+	srand((unsigned) time(&t)); //initilize random number generator
+
+	int *array_A2 = (int*)malloc(row_A_size*column_A_size*sizeof(int));
+	for(int i=0; i<row_A_size; i++)
+	{
+		for(int j=0; j<column_A_size; j++)
+		{
+			*(array_A2 + i*column_A_size + j) = (rand() % 20);
+		}
+	}
+
+	printf("dynamic array contents:\n");
+	for(int i=0; i<row_A_size; i++)
+	{
+		for(int j=0; j<column_A_size; j++)
+		{
+			printf("%d ",*(array_A2 + i*column_A_size + j));
+		}
+		printf("\n");
+	}
+	printf("\n");
+	int *array_B2 = (int*)malloc(row_B_size*column_B_size*sizeof(int));
+	for(int i=0; i<row_B_size; i++)
+	{
+		for(int j=0; j<column_B_size; j++)
+		{
+			*(array_B2 + i*column_B_size + j) = (rand() % 20);
+		}
+	}
+
+	printf("dynamic array contents:\n");
+	for(int i=0; i<row_B_size; i++)
+	{
+		for(int j=0; j<column_B_size; j++)
+		{
+			printf("%d ",*(array_B2 + i*column_B_size + j));
+		}
+		printf("\n");
+	}
+	/*********************************************************************************/
 	int array_A[row_A_size][column_A_size] = { {1,2,3,4},{5,6,7,8},{9,10,11,12} };
 	int array_B[row_B_size][column_B_size] = { {2,3,5},{7,11,13},{17,19,23},{29,31,37} };
 
@@ -62,7 +104,6 @@ int main(void)
 	}
 	printf("\n");
 
-	printf("entering struct declaration and definition\n");
 	/********************************************************************************/
 	/** Create struct to pass multiple arguments to pthread_create() ***************/
 	struct matrix_multiplication_struct arguments;
@@ -71,16 +112,24 @@ int main(void)
 	arguments.array2 = &array_B;
 	arguments.array3 = &array_C;
 	/*******************************************************************************/
-	printf("exiting struct declaration and definition\n");
-	printf("\n");
-
 	/** Test arguments->arrayx *****************************************************/
-	printf("arguments->array1:\n");
+	printf("matrix_multiplication_struct arguments->array1:\n");
 	for(int i=0; i<row_A_size; i++)
 	{
 		for(int j=0; j<column_A_size; j++)
 		{
 			printf("%d ", (*arguments.array1)[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+	printf("matrix_multiplication_struct arguments->array2:\n");
+	for(int i=0; i<row_B_size; i++)
+	{
+		for(int j=0; j<column_B_size; j++)
+		{
+			printf("%d ", (*arguments.array2)[i][j]);
 		}
 		printf("\n");
 	}
@@ -94,12 +143,12 @@ int main(void)
 		printf("Could not create mutex, err=%s.\n", strerror(err));
 	/********************************************************************************/
 	/** Create threads, join them with main thread  and lock them *******************/
-	printf("creating, joining and locking threads.................\n");
-
+	printf("creating, joining, and locking threads.................\n");
+	printf("\n");
 	/** thread 1 ********************************************************/
 	err1 = pthread_mutex_lock(lock);
 	if(err1 !=0)
-		printf("Error creating mutex, err=%s.\n", strerror(err1));
+		printf("Error locking mutex, err=%s.\n", strerror(err1));
 
 	arguments.row_index = 0; 
 	err=pthread_create(&thread_1, NULL, dot_product, (void *)&arguments);
@@ -112,12 +161,12 @@ int main(void)
 
 	err1 = pthread_mutex_unlock(lock);
 	if(err1 !=0)
-		printf("Error creating mutex, err=%s.\n", strerror(err1));
+		printf("Error unlocking mutex, err=%s.\n", strerror(err1));
 
 	/** thread 2 ********************************************************/
 	err1 = pthread_mutex_lock(lock);
 	if(err1 !=0)
-		printf("Error creating mutex, err=%s.\n", strerror(err1));
+		printf("Error locking mutex, err=%s.\n", strerror(err1));
 
 	arguments.row_index = 1; 
 	err = pthread_create(&thread_2, NULL, dot_product, (void *)&arguments);
@@ -130,12 +179,12 @@ int main(void)
 
 	err1 = pthread_mutex_unlock(lock);
 	if(err1 !=0)
-		printf("Error creating mutex, err=%s.\n", strerror(err1));
+		printf("Error unlocking mutex, err=%s.\n", strerror(err1));
 
 	/** thread 3 ********************************************************/
 	err1 = pthread_mutex_lock(lock);
 	if(err1 !=0)
-		printf("Error creating mutex, err=%s.\n", strerror(err1));
+		printf("Error locking mutex, err=%s.\n", strerror(err1));
 
 	arguments.row_index = 2; 
 	err = pthread_create(&thread_3, NULL, dot_product, (void *)&arguments);
@@ -148,17 +197,16 @@ int main(void)
 
 	err1 = pthread_mutex_unlock(lock);
 	if(err1 !=0)
-		printf("Error creating mutex, err=%s.\n", strerror(err1));
+		printf("Error unlocking mutex, err=%s.\n", strerror(err1));
 
-	printf("\n");
 	/********************************************************************************/
 	/** print exit codes of each thread *********************************************/
 
-	printf("Thread %d has exit code %ld.\n", (int)thread_1, (long)tret1);
-	printf("Thread %d has exit code %ld.\n", (int)thread_2, (long)tret2);
-	printf("Thread %d has exit code %ld.\n", (int)thread_3, (long)tret3);
+	//printf("Thread %d has exit code %ld.\n", (int)thread_1, (long)tret1);
+	//printf("Thread %d has exit code %ld.\n", (int)thread_2, (long)tret2);
+	//printf("Thread %d has exit code %ld.\n", (int)thread_3, (long)tret3);
 
-	printf("\n");
+	//printf("\n");
 	/********************************************************************************/
 	/** Print contents of matrix_C **************************************************/
 	printf("matrix_C:\n");
@@ -176,7 +224,7 @@ int main(void)
 	return 0;
 }
 
-/** Function performs dot product multiplication or given row from array1********************************************************** **/
+/** Function performs dot product multiplication or given row from array1***************************************/
 
 void *dot_product(void *arguments)
 {
@@ -187,26 +235,22 @@ void *dot_product(void *arguments)
 	int *array2;
 	int *array3;
 
-	int row_index;
+	int row_index; //row index of array1, column index of array2
 ************************************/
-	printf("Row Index: %d\n", args->row_index);
+ 	printf("In dot_product function, row_index = %d\n", args->row_index);
 	int sum = 0;	
 	for(int j=0; j<column_B_size; j++)
 	{
-		/** column index for first matrix and row index for second matrix **/
+		/** k = column index for first matrix and row index for second matrix **/
 		for(int k=0; k<column_A_size; k++)
 		{
-			//sum += array1[row_index][k] * array2[k][j];
 			sum += (*args->array1)[(args->row_index)][k] * (*args->array2)[k][j];
-
-			//printf("testing: j=%d, k=%d array1[][]= %d\n", j, k, (*args->array1)[args->row_index][k]);
 		}
 	
-		//array3[args->row_index][j] = sum;
 		(*args->array3)[args->row_index][j] = sum;		
 		sum = 0;
 	}
-	printf("\n");
+
 	pthread_exit((void*) 0);
 	return 0;
 }
